0417b16860f786003af0921a194d78cb4c4dec59
sherifahmedeisa <149529914+sherifahmedeisa@users.noreply.github.com>
2026-08-31 16:28:07 +0300

    first commit (cart intiail)

---
+++ js/cart.js
@@ -0,0 +1,17 @@
+// // this file will handle the cart adding asuming that the items pre-saved in localstorage by name cart
+// author :Sherif Ahmed
+
+
+// mock to delete after real implemntation
+localStorage.setItem("image", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9y_CLYQ3Gj-cDf426qm_CdkU-d42yxXIEN-o0-St9_Q&s=10");
+localStorage.setItem("product name", "Bar chair");
+localStorage.setItem("pric", "100");
+localStorage.setItem("Quantity", "0");
+
+
+
+
+function setQuantity( qnt){
+     
+}
+
